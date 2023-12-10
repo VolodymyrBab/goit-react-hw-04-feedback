778e6bee24b2e2f4e9d5ba57b7646e6090011e87
VolodymyrBab <volbv@yahoo.com>
2023-12-10 13:27:48 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, {useState } from "react";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Section from "./Sections/Section";
 import Statistics from "./Statistics/Statistics";
